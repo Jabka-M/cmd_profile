aca4f561ffe6ea88c417a0d04a0e955a0f3dabf8
Jabka-M <discordbotcreator@mail.ru>
2024-01-13 17:02:07 +0300

    added setup file for current user only

---
+++ install_for_current_user.cmd
@@ -0,0 +1,6 @@
+::@echo off
+:: MADE BY Jabka-M (GitHub)
+
+curl https://raw.githubusercontent.com/Jabka-M/cmd_profile/main/cmd_profile.cmd > %USERPROFILE%\cmd_profile.cmd
+
+reg add "HKCU\SOFTWARE\Microsoft\Command Processor" /v "AutoRun" /t REG_SZ /d "%USERPROFILE%\cmd_profile.cmd" /f
